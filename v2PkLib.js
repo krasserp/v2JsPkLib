c0698bdf697e76518a8bb039e530f7aae11e48a9
pkrasser@decipherinc.com <pkrasser@beacon.decryptresearch.co.uk>
2015-04-17 11:49:48 +0100

    initial push

---
+++ v2PkLib.js
@@ -0,0 +1,157 @@
+(function ( $ ) {
+    // ok just setting to pQ to avoid jQuery or $ ()
+    window.jQuery = window.pQ = jQuery;
+    $.fn.doesExist = function(item){
+        return this.length > 0;
+    };
+    $.fn.helper = function(action,options ) {
+
+        // basic IE console error prevention
+        if(typeof(console) === 'undefined') {
+            console = function(){};
+            console.log = function(){
+                consoleMsg();
+            };
+        }
+        // This is the easiest way to have default options.
+        var settings = $.extend({
+            // These are the defaults.
+            from: 0,
+            to: 2,
+            imgTableCellSelector: '.sq-atm1d-table-cell',
+            imgArr: [ 'http://static.businessinsider.com/image/51963a396bb3f77b52000004/image.jpg',
+                        'http://microlancer.lancerassets.com/uploads/service_attachments/7509/thumbnail_agile_mascot-1369328743.jpg',
+                        'http://s3.amazonaws.com/wbstaticfiles/users/929/ts_244336_dog-hed-1-bwpng.jpg'
+                       ],
+            testingMsg : 'Do not rush this testing',
+            subBtnSelector: '#btn_continue, #btn_finish',
+            vTimeout : 5,
+            allowedExtensions : ['.jpg','.png','.doc','docx','.pdf','.gif'],
+            extensionErrorMsg : 'Please only upload files of the following file types:',
+        },
+        options );
+        /********************** FIRST some Helpers ************************/
+        /**
+         * return any value between two values 
+         * @from  int
+         * @to int
+         * @return int
+         */
+        var randomFromToInterval = function(from,to)
+            {
+                return Math.floor(Math.random()*(settings.to-settings.from+1)+settings.from);
+            };
+
+        
+        /**
+         * get the input field type
+         * @return {string}
+         */
+        var getType = function()
+            {
+                return this[0].tagName == "INPUT" ? this[0].type.toLowerCase() : this[0].tagName.toLowerCase();
+            };
+        /**
+         * [enable disable ]
+         * @param  {[type]} in 0= off 1= on
+         * @return {[type]} nothing
+         */
+        var disEnableSubmit = function(para){
+            var b = $(settings.subBtnSelector);
+                if (para===0){
+                    b.disable();
+                    b.addClass('hidden');
+                    console.log('disable',$('#btn_continue').attr('class') );
+
+                } else if (para ===1){
+                    b.enable();
+                    b.removeClass('hidden');
+                }
+        };
+        
+        /******************************* makeImbBtn ********************************/
+        /**
+         * call if you want atm1d.6 buttons to be an image // note the items will need to include imges as values // default selector is atm1d.6 class see settings
+         * @param  {[type]}
+         * @return {[type]}
+         */
+        if(action ==='makeImbBtn'){
+            var t = setTimeout(function(){
+              $(settings.imgTableCellSelector).each(function(){
+                var imgSrc = $ (this).find('img').attr('src');
+                $ (this).find('img').css('display','none');
+                $ (this).find('.sq-atm1d-btn-bg').css("background-image","url('"+imgSrc+"')");
+                $ (this).find('.sq-atm1d-btn-bg').css("background-size","cover");
+              });
+            },500);
+        }
+
+        /**
+         * prevent super rushed QA's 
+         * @param  none  // change imgs in settings.imgArr
+         * @return max fun
+         */
+        if(action ==='hillarious') {
+
+            var hillarious= function(){
+                var alt = false, c = false, imgArr = settings.imgArr,from = 0,to = settings.imgArr.length;
+                var number = randomFromToInterval(from, to);
+                $(document).keyup(function(e) {
+                      if(e.which == 17) {
+                        alt = false;
+                      }
+                });
+                $(document).keydown(function(e) {
+                      if(e.which == 17) {
+                        alt = true;
+                      }
+                    if(alt && e.which == 39) {
+                      $ ('#surveyContent').html(
+                        '<div class="pkErrorMsg"> '+settings.testingMsg+' <br> <img src="'+imgArr[number]+'" /><div>');
+                    }
+                });
+            }();
+        }
+
+
+        /************************************* videoSkip ***********************************/
+        if(action ==='videoSkip'){
+            /**
+             * Allows via setting (settings.vTimeout)a timeout to not force the user to view the whole video
+             * @return enabled continue or submit btn
+             */
+            var videoSkip = function(){
+                disEnableSubmit(0);
+                setTimeout(function() { disEnableSubmit(1);} , settings.vTimeout*1000);
+                $('input:checkbox').prop( "checked", true );
+                }();
+        }
+
+        /************************************ fileExtensionCheck *************************/
+
+        if(action==='fileExtensionCheck'){
+            $('input[type="file"]').change(function(){
+                var that = $(this);
+                fileExtensionCheck(settings.allowedExtensions, that);
+              });
+
+            var fileExtensionCheck = function(extensionList, that){
+                var extension = that.val().substr(that.length - 5);
+                var errorMsg = settings.extensionErrorMsg + extensionList.join();
+                if( $.inArray(extension.toLowerCase(), extensionList)== -1){
+                  that.addClass('pkErrorMsg');
+                  alert(errorMsg);
+                  disEnableSubmit(0);
+                }
+                else{
+                  that.removeClass('pkErrorMsg');
+                  disEnableSubmit(1);
+                }
+              };
+
+        }
+
+ 
+    };
+ 
+}( jQuery ));
